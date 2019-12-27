c29f0c1bf1923167154a0b8282126072190a8f3d
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2019-12-27 23:29:57 +0100

    added post callback hell

--- routes/users.js
+++ routes/users.js
@@ -9,32 +9,53 @@ const config = {
   database: 'fmClashRoyale', //(Nome del DB)
 }
 
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   sql.connect(config, err => {
-    if(err) console.log(err);  // ... error check
-    
+    if (err) console.log(err);  // ... error check
+
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
     sqlRequest.query('select * from dbo.[cr-unit-attributes]', (err, result) => {
-        if (err) console.log(err); // ... error checks
-        res.send(result);  //Invio il risultato
+      if (err) console.log(err); // ... error checks
+      res.send(result);  //Invio il risultato
     });
   });
 });
 
-router.get('/search/:name', function(req, res, next) {
+router.get('/search/:name', function (req, res, next) {
   sql.connect(config, err => {
     // ... error check
-    if(err) console.log(err);
+    if (err) console.log(err);
     // Query
     let sqlRequest = new sql.Request();
     sqlRequest.query(`select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`, (err, result) => {
-        // ... error checks
-        if (err) console.log(err);
-        res.send(result);
+      // ... error checks
+      if (err) console.log(err);
+      res.send(result);
     });
   });
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  // Add a new Unit  
+  let unit = req.body;
+  if (!unit) {
+    return res.status(400).send({ error: true, message: 'Please provide a unit' });
+  }
+  sql.connect(config, err => {
+    let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
+                     VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
+    console.log(sqlInsert);
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(sqlInsert, (error, results) => {
+      if (error) throw error;
+      return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+  })
 });
 
 module.exports = router;
