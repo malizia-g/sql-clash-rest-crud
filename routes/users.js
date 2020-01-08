68a607b8848f2de2e79b6c5eca8f9275491a216a
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2020-01-08 10:58:02 +0100

    aggiunta form pug

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,31 @@
 var express = require('express');
 var router = express.Router();
 const sql = require('mssql');
-var createError = require('http-errors');
 
 const config = {
   user: 'malizia.fabio',  //Vostro user name
   password: 'scuola2019*', //Vostra password
-  server: "213.140.22.237\\sqlexpress",  //Stringa di connessione
+  server: "213.140.22.237",  //Stringa di connessione
   database: 'fmClashRoyale', //(Nome del DB)
 }
 
 //Function to connect to database and execute query
 let executeQuery = function (res, query, next) {
   sql.connect(config, function (err) {
-    if (err) {
+    if (err) { //Display error page
       console.log("Error while connecting database :- " + err);
-      next(createError(500, "Internal server error")); //Display error page
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
       return;
     }
     var request = new sql.Request(); // create Request object
     request.query(query, function (err, result) { //Display error page
       if (err) {
         console.log("Error while querying database :- " + err);
-        next(createError(500, "Internal server error")); //On error
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
         sql.close();
         return;
       }
-      res.send(result);
+      res.send(result.recordset);
       sql.close();
     });
 
@@ -44,15 +43,19 @@ router.get('/search/:name', function (req, res, next) {
   executeQuery(res, sqlQuery, next);
 });
 
+
 router.post('/', function (req, res, next) {
   // Add a new Unit  
   let unit = req.body;
+  console.log(unit);
   if (!unit) {  //Qui dovremmo testare tutti i campi della richiesta
-    return next(createError(400, "Please provide a correct unit"));
+    res.status(500).json({success: false, message:'Error while connecting database', error:err});
+    return;
   }
   let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
                      VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
   executeQuery(res, sqlInsert, next);
+  res.send({success:true, message: "unità inserita con successo", unit: unit})
 });
 
 module.exports = router;
